ea87ced61d93e6a20537fb6728fc91b7d0493c89
George <gkilmain@gmail.com>
2018-06-05 22:16:23 -0400

    CompletedQueryIntentHandler and mock data.

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,24 +1,9 @@
 /* eslint-disable  func-names */
 /* eslint-disable  no-console */
-
+const json = require('./people.json');
 const Alexa = require('ask-sdk-core');
 
-const StartQueryIntentHandler = {
-  canHandle(handlerInput){
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'StartQuery';
-  },
-  handle(handlerInput){
-    const speechText = "Okay, let's get started building a list.";
-    console.log('This called: ', this)
-
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .getResponse()
-  }
-}
-
-const InProgressStartQueryIntentHandler = {
+const InProgressQueryIntentHandler = {
   canHandle(handlerInput) {
     const request = handlerInput.requestEnvelope.request;
     console.log('This called: ', this)
@@ -33,6 +18,24 @@ const InProgressStartQueryIntentHandler = {
       .getResponse();
   },
 };
+
+const CompletedQueryIntentHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest' && request.intent.name === 'StartQuery';
+  },
+  handle(handlerInput){
+    const responseBuilder = handlerInput.responseBuilder;
+    const filledSlots = handlerInput.requestEnvelope.request.intent.slots;
+    const slotValues = getSlotValues(filledSlots);
+    const data = json.people.filter((person) => (person.company.toLowerCase() === slotValues.COMPANY_NAME.resolved.toLowerCase() && person.address.city.toLowerCase() === 'Boston'.toLocaleLowerCase()))
+    
+    return handlerInput.responseBuilder
+    .speak(`I found ${data.length} people that match your criteria. I've sent the data as a skill card for you to see on your Alexa app.`)
+    .withSimpleCard('Your Criteria List', JSON.stringify(data))
+    .getResponse()
+  }
+}
 
 const LaunchRequestHandler = {
   canHandle(handlerInput) {
@@ -114,8 +117,50 @@ exports.handler = skillBuilder
     HelpIntentHandler,
     CancelAndStopIntentHandler,
     SessionEndedRequestHandler,
-    StartQueryIntentHandler,
-    InProgressStartQueryIntentHandler
+    InProgressQueryIntentHandler,    
+    CompletedQueryIntentHandler
   )
   .addErrorHandlers(ErrorHandler)
   .lambda();
+
+  function getSlotValues(filledSlots) {
+    const slotValues = {};
+  
+    console.log(`The filled slots: ${JSON.stringify(filledSlots)}`);
+    Object.keys(filledSlots).forEach((item) => {
+      const name = filledSlots[item].name;
+  
+      if (filledSlots[item] &&
+        filledSlots[item].resolutions &&
+        filledSlots[item].resolutions.resolutionsPerAuthority[0] &&
+        filledSlots[item].resolutions.resolutionsPerAuthority[0].status &&
+        filledSlots[item].resolutions.resolutionsPerAuthority[0].status.code) {
+        switch (filledSlots[item].resolutions.resolutionsPerAuthority[0].status.code) {
+          case 'ER_SUCCESS_MATCH':
+            slotValues[name] = {
+              synonym: filledSlots[item].value,
+              resolved: filledSlots[item].resolutions.resolutionsPerAuthority[0].values[0].value.name,
+              isValidated: true,
+            };
+            break;
+          case 'ER_SUCCESS_NO_MATCH':
+            slotValues[name] = {
+              synonym: filledSlots[item].value,
+              resolved: filledSlots[item].value,
+              isValidated: false,
+            };
+            break;
+          default:
+            break;
+        }
+      } else {
+        slotValues[name] = {
+          synonym: filledSlots[item].value,
+          resolved: filledSlots[item].value,
+          isValidated: false,
+        };
+      }
+    }, this);
+  
+    return slotValues;
+  }
